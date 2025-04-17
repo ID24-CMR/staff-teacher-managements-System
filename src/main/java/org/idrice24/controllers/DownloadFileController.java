a0ef90d7b3c3487e2dff80ec9fa7cbdf54bb1027
ID24-CMR <idrice.tsafouet@yahoo.com>
2025-04-17 18:32:03 +0200

    update files

--- src/main/java/org/idrice24/controllers/DownloadFileController.java
+++
@@ -1,8 +0,0 @@
-package org.idrice24.controllers;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class DownloadFileController {
-    
-}
--- src/main/java/org/idrice24/repositories/DepartmentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.idrice24.repositories;
-
-import org.idrice24.entities.Department;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DepartmentRepository extends CrudRepository<Department, Long> {
-
-}
